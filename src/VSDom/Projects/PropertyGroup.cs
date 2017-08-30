51503a73a9defb2942994450e5ff440ac7dd3a03
Corniel Nobel <mail@corniel.nl>
2017-08-30 08:54:18 +0200

    Added CodeAnalysisRuleSet to property group.

--- src/VSDom/Projects/PropertyGroup.cs
+++ src/VSDom/Projects/PropertyGroup.cs
@@ -15,85 +15,92 @@ namespace VSDom.Projects
 		public PropertyGroup(XElement element) : base(element) { }
 
 		/// <summary>Gets the local name of the <see cref="PropertyGroup"/>.</summary>
-		public override string LocalName { get { return "PropertyGroup"; } }
+		public override string LocalName => nameof(PropertyGroup);
 
-		/// <summary>Returns true if the the property group describes a specific (debug or release) configuration.</summary>
-		public bool IsSpecificConfiguration { get { return IsDebugConfiguration || IsReleaseConfiguration; } }
+		/// <summary>Returns true if the property group describes a specific (debug or release) configuration.</summary>
+		public bool IsSpecificConfiguration => IsDebugConfiguration || IsReleaseConfiguration;
 
-		/// <summary>Returns true if the the property group describes a debug configuration.</summary>
-		public bool IsDebugConfiguration { get { return !string.IsNullOrEmpty(Condition) && Condition.ToUpperInvariant().Contains("DEBUG"); } }
+		/// <summary>Returns true if the property group describes a debug configuration.</summary>
+		public bool IsDebugConfiguration => !string.IsNullOrEmpty(Condition) && Condition.ToUpperInvariant().Contains("DEBUG");
 
-		/// <summary>Returns true if the the property group describes a release configuration.</summary>
-		public bool IsReleaseConfiguration { get { return !string.IsNullOrEmpty(Condition) && Condition.ToUpperInvariant().Contains("RELEASE"); } }
+		/// <summary>Returns true if the property group describes a release configuration.</summary>
+		public bool IsReleaseConfiguration => !string.IsNullOrEmpty(Condition) && Condition.ToUpperInvariant().Contains("RELEASE");
 
 		/// <summary>Gets and set the condition for the property group.</summary>
 		public string Condition
 		{
-			get { return Get("Condition", true); }
-			set { Set("Condition", value, true); }
+			get => Get(nameof(Condition), true);
+			set => Set(nameof(Condition), value, true);
 		}
-		
+
+		/// <summary>Gets and set the location of the rule set.</summary>
+		public string CodeAnalysisRuleSet
+		{
+			get => Get(nameof(CodeAnalysisRuleSet));
+			set => Set(nameof(CodeAnalysisRuleSet), value);
+		}
+
 		/// <summary>Gets and set the debug symbols value.</summary>
 		public string DebugSymbols
 		{
-			get { return Get("DebugSymbols"); }
-			set { Set("DebugSymbols", value); }
+			get => Get(nameof(DebugSymbols));
+			set => Set(nameof(DebugSymbols), value);
 		}
 
 		/// <summary>Gets and set the debug type.</summary>
 		public string DebugType
 		{
-			get { return Get("DebugType"); }
-			set { Set("DebugType", value); }
+			get => Get(nameof(DebugType));
+			set => Set(nameof(DebugType), value);
 		}
 
 		/// <summary>Gets and set the defined constants.</summary>
 		public string DefineConstants
 		{
-			get { return Get("DefineConstants"); }
-			set { Set("DefineConstants", value); }
+			get => Get(nameof(DefineConstants));
+			set => Set(nameof(DefineConstants), value);
 		}
 
 		/// <summary>Gets and set the documentation file.</summary>
 		public string DocumentationFile
 		{
-			get { return Get("DocumentationFile"); }
-			set { Set("DocumentationFile", value); }
+			get => Get(nameof(DocumentationFile));
+			set => Set(nameof(DocumentationFile), value);
 		}
 
 		/// <summary>Gets and set the error report.</summary>
 		public string ErrorReport
 		{
-			get { return Get("ErrorReport"); }
-			set { Set("ErrorReport", value); }
+			get => Get(nameof(ErrorReport));
+			set => Set(nameof(ErrorReport), value);
 		}
 
 		/// <summary>Gets and set optimize.</summary>
 		public string Optimize
 		{
-			get { return Get("Optimize"); }
-			set { Set("Optimize", value); }
+			get => Get(nameof(Optimize));
+			set => Set(nameof(Optimize), value);
 		}
 
 		/// <summary>Gets and set the output path.</summary>
 		public string OutputPath
 		{
-			get { return Get("OutputPath"); }
-			set { Set("OutputPath", value); }
+			get => Get(nameof(OutputPath));
+			set => Set(nameof(OutputPath), value);
 		}
-		
+
 		/// <summary>Gets and set the preference for 32 bit.</summary>
 		public bool? Prefer32Bit
 		{
-			get { return GetBoolean("Prefer32Bit"); }
-			set { SetBoolean("Prefer32Bit", value); }
+			get => GetBoolean(nameof(Prefer32Bit));
+			set => SetBoolean(nameof(Prefer32Bit), value); 
 		}
 
 		/// <summary>Gets and set the warning level.</summary>
 		public string WarningLevel
 		{
-			get { return Get("WarningLevel"); }
-			set { Set("WarningLevel", value); }
+			get => Get(nameof(WarningLevel));
+			set => Set(nameof(WarningLevel), value);
 		}
 	}
 }
